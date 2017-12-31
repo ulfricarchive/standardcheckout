1e5e74b110c535ea7710cf35add347f4893d4828
Adam Harrison <ad@mharrison.us>
2017-12-30 19:24:39 -0500

    Add the rest of the countries

--- src/main/java/com/standardcheckout/web/stripe/Country.java
+++ src/main/java/com/standardcheckout/web/stripe/Country.java
@@ -3,7 +3,29 @@ package com.standardcheckout.web.stripe;
 public enum Country {
 
 	UNITED_STATES("United States", "US"),
-	AUSTRALIA("Australia", "AU");
+	AUSTRALIA("Australia", "AU"),
+	AUSTRIA("Austria", "AT"),
+	BELGIUM("Belgium", "BE"),
+	BRAZIL("Brazil", "BR"),
+	CANADA("Canada", "CA"),
+	DENMARK("Denmark", "DK"),
+	FINLAND("Finland", "FI"),
+	FRANCE("France", "FR"),
+	GERMANY("Germany", "DE"),
+	HONG_KONG("Hong Kong", "HK"),
+	IRELAND("Ireland", "IE"),
+	//ITALY("Italy", "IT"),
+	JAPAN("Japan", "JP"),
+	LUXEMBOURG("Luxembourg", "LU"),
+	//MEXICO("Mexico", "MX"),
+	NETHERLANDS("Netherlands", "NL"),
+	NEW_ZEALAND("New Zealand", "NZ"),
+	NORWAY("Norway", "NO"),
+	//PORTUGAL("Portugal", "PT"),
+	SINGAPORE("Singapore", "SG"),
+	SPAIN("Spain", "ES"),
+	SWEDEN("Sweden", "SE"),
+	UNITED_KINGDOM("United Kingdom", "UK");
 
 	private final String friendly;
 	private final String twoLetter;
