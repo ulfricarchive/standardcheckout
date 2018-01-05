da6370ba21db1f3813d3df283331f20e407d3277
Adam Harrison <ad@mharrison.us>
2018-01-05 10:41:46 -0500

    Cleanuop

--- src/main/java/com/standardcheckout/web/helper/StringHelper.java
+++ src/main/java/com/standardcheckout/web/helper/StringHelper.java
@@ -1,13 +1,8 @@
 package com.standardcheckout.web.helper;
 
-import org.jclouds.apis.Apis;
 import org.springframework.util.StringUtils;
 
 public class StringHelper {
-
-	public static void main(String[] args) {
-		Apis.all().forEach(System.out::println);
-	}
 
 	public static boolean isInBounds(String value, int lower, int upper) {
 		if (StringUtils.isEmpty(value)) {
